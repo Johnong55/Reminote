a078dffd6c2a4a7cfaa47265f97ba4c7c84ef415
Johnong55 <trijohn124@gmail.com>
2025-05-06 07:32:28 +0700

    update habit into cloud using firebase as cloud storage

---
+++ lib/Online_Repository/Habit_Repository_Online.dart
@@ -0,0 +1,109 @@
+
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:study_app/models/Habit.dart';
+
+class HabitRepoistoryOnline{
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  String? get userID => _auth.currentUser?.uid;
+  // add habit to firebase
+  Future<void> addHabitToFirebase(Habit habit) async{
+    try{
+      if(userID == null) throw Exception("User haven't log in");
+      final habitRef = await _firestore
+          .collection("users")
+          .doc(userID)
+          .collection("Habits")
+          .add({
+            'title':habit.title,
+            'description':habit.description,
+            'due_time': habit.due_time,
+            'due_date': habit.due_date,
+            'isCompleted': habit.isCompleted,
+            'frequency_type': habit.frequency_type,
+            'target_count': habit.target_count,
+            'start_date': habit.start_date,
+            'color': habit.color,
+            'userEmail': _auth.currentUser?.email,
+            
+          });
+         habit.ID = habitRef.id;
+    }
+    catch(e){
+      log("Error adding habit: $e");
+    }
+  }
+  Future<List<Habit>> getHabitsByUseEmail() async{
+    try{
+      if(this.userID == null){
+        throw Exception("User haven't log in");
+      } 
+      final habitSnapshot = await _firestore
+          .collection("users")
+          .doc(userID)
+          .collection("Habits")
+          .get();
+      return habitSnapshot.docs.map((doc){
+        final data = doc.data();
+        return Habit(title: data['title'],
+            description: data['description'],
+            due_time: data['due_time'],
+            due_date: data['due_date'],
+            isCompleted: data['isCompleted'],
+            frequency_type: data['frequency_type'],
+            target_count: data['target_count'],
+            start_date: data['start_date'],
+            color: data['color'],
+            userEmail: data['userEmail']
+        )..ID = doc.id;
+      }).toList();
+      }
+      catch(e){
+        log("Error getting habits: $e");
+        return [];
+      }
+    }
+    Future<void> updateHabitToFirebase(Habit habit) async{
+      try{
+        if(userID == null && habit.ID == null) throw Exception("User haven't log in or habit ID is null");
+        await _firestore
+            .collection("users")
+            .doc(userID)
+            .collection("Habits")
+            .doc(habit.ID)
+            .update({
+              'title': habit.title,
+              'description': habit.description,
+              'due_time': habit.due_time,
+              'due_date': habit.due_date,
+              'isCompleted': habit.isCompleted,
+              'frequency_type': habit.frequency_type,
+              'target_count': habit.target_count,
+              'start_date': habit.start_date,
+              'color': habit.color,
+              'userEmail': _auth.currentUser?.email,
+              
+            });
+      }
+      catch(e){
+        log("Error updating habit: $e");
+      }
+    }
+    Future<void> deleteHabitToFirebase(Habit habit) async{
+      try{
+        if(userID == null && habit.ID == null) throw Exception("User haven't log in or habit ID is null");
+        await _firestore
+            .collection("users")
+            .doc(userID)
+            .collection("Habits")
+            .doc(habit.ID)
+            .delete();
+      }
+      catch(e){
+        log("Error deleting habit: $e");
+      }
+    }
+  }
--- lib/services/HabitService.dart
+++ lib/services/HabitService.dart
@@ -1,49 +1,102 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:study_app/Offline_Repository/Habit_repository.dart';
+import 'package:study_app/Online_Repository/Habit_Repository_Online.dart';
 import 'package:study_app/models/Habit.dart';
 import 'package:study_app/services/CompletionService.dart';
 
 
 class HabitService {
-  final HabitRepository _habitRepository  = HabitRepository();
+  final HabitRepository _offlineRepo  = HabitRepository();
   final CompletionService completionService = CompletionService();
+  final HabitRepoistoryOnline _onlineRepo = HabitRepoistoryOnline();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final Connectivity _connectivity = Connectivity();
+  bool _isInitialized = false;
+  late final CollectionReference _cloudCollection;
+  
   
 
   // Initialize the database
   Future<void> init() async {
-    await _habitRepository.initializeIsar();
+    if(_isInitialized) return;
+
+    
+    await _offlineRepo.initializeIsar();
+    final user = _auth.currentUser;
+    _cloudCollection = FirebaseFirestore.instance.collection(
+      user != null ? 'users/${user.uid}/Habits' : 'Habits',
+    );
+    if(user == null) {
+      print("Warning: User not logged in - Firestore may reject operations");
+    }
+    _isInitialized = true;
   }
+  // check Authentication
+  bool _checkAuth(){
+    final user = _auth.currentUser;
+    if(user == null) {
+      print("Error: User not logged in");
+      return false;
+    }
+    return true;
+  }
+  // check Network
+  Future<bool> _isNetworkAvailable() async {
+    final result = await _connectivity.checkConnectivity();
+    return result != ConnectivityResult.none;
+  }
+
 
   // Create a habit
   Future<void> addHabit(Habit habit) async {
-    await _habitRepository.createHabit(habit);
+    habit.ID = "local_${DateTime.now().millisecondsSinceEpoch}";
+    habit.userEmail = _auth.currentUser?.email ?? "local_user";
+
+    await _offlineRepo.createHabit(habit);
+    if(await _isNetworkAvailable() && _onlineRepo.userID !=null){
+      try{
+        habit.userEmail = _auth.currentUser?.email;
+        await _onlineRepo.addHabitToFirebase(habit);
+        
+      } catch(e){
+        log("Error adding habit to Firebase: $e");
+        habit.userEmail = "local_user";
+        await _offlineRepo.updateHabit(habit);
+      }
+    }
   }
 
+
   // Get all habits
-  List<Habit> get allHabits => _habitRepository.currenthabits;
+  List<Habit> get allHabits => _offlineRepo.currenthabits;
 
   // Fetch all habits from DB
   Future<void> refreshHabits() async {
-    await _habitRepository.fetchhabits();
+    await _offlineRepo.fetchhabits();
   }
 
   // Get a habit by ID
   Future<Habit?> getHabitById(int id) async {
-    return await _habitRepository.getHabitById(id);
+    return await _offlineRepo.getHabitById(id);
   }
 
   // Update an existing habit
   Future<void> updateHabit(Habit habit) async {
-    await _habitRepository.updateHabit(habit);
+    await _offlineRepo.updateHabit(habit);
   }
 
   // Delete a habit by ID
   Future<void> deleteHabit(int id) async {
-    await _habitRepository.deleteHabit(id);
+    await _offlineRepo.deleteHabit(id);
   }
 
   // Delete all habits
   Future<void> clearAllHabits() async {
-    await _habitRepository.deleteAllhabits();
+    await _offlineRepo.deleteAllhabits();
   }
 
   // Toggle a habit's completion state
@@ -53,41 +106,92 @@ class HabitService {
 
   // Get habits for a specific day (including repeating ones)
   Future<List<Habit>> getHabitsForDay(DateTime date) async {
-    return await _habitRepository.getListHabitInSpecDay(date);
+    return await _offlineRepo.getListHabitInSpecDay(date);
   }
 
   // Get only daily habits
   Future<List<Habit>> getDailyHabits(DateTime date) async {
-    return await _habitRepository.getListDailyHabits(date);
+    return await _offlineRepo.getListDailyHabits(date);
   }
 
   // Get only weekly habits
   Future<List<Habit>> getWeeklyHabits(DateTime date) async {
-    return await _habitRepository.getListWeeklyHabit(date);
+    return await _offlineRepo.getListWeeklyHabit(date);
   }
 
   // Get only monthly habits
   Future<List<Habit>> getMonthlyHabits(DateTime date) async {
-    return await _habitRepository.getListMonthlyHabits(date);
+    return await _offlineRepo.getListMonthlyHabits(date);
   }
 
   // Get habits filtered by completion
   Future<List<Habit>> getHabitsByCompletion(bool isCompleted) async {
-    return await _habitRepository.findByIsCompleted(isCompleted);
+    return await _offlineRepo.findByIsCompleted(isCompleted);
   }
 
   // Get habits in a date range
   Future<List<Habit>> getHabitsByDateRange(DateTime start, DateTime end) async {
-    return await _habitRepository.findByDateRange(start, end);
+    return await _offlineRepo.findByDateRange(start, end);
   }
 
   // Count completed habits for a specific day
   Future<int> countCompletedForDay(DateTime date) async {
-    return await _habitRepository.countCompletedhabitsByDay(date);
+    return await _offlineRepo.countCompletedhabitsByDay(date);
   }
 
   // Check if all habits for a day are completed
   Future<bool> areAllHabitsCompleted(DateTime date) async {
-    return await _habitRepository.areAllhabitsCompletedForDay(date);
+    return await _offlineRepo.areAllhabitsCompletedForDay(date);
+  }
+  Future<void> pullHabitIntoLocal() async {
+    if (await _isNetworkAvailable() && _onlineRepo.userID != null) {
+      try {
+        final user = _auth.currentUser;
+        if(user == null){
+          log("Error: User not logged in");
+          return;
+        }
+        final snapshot  = await _cloudCollection.get();
+        final List<Habit> habits = snapshot.docs.map((doc) {
+          final data = doc.data() as Map<String, dynamic>;
+          return Habit.fromJson(data);
+        }).toList();
+        for (var habit in habits) {
+          habit.userEmail = user.email;
+          await _offlineRepo.createHabit(habit);
+        }
+      } catch (e) {
+        log("Sync failed: $e");
+      }
+    } else {
+      log(_onlineRepo.userID == null ? "Login required to sync" : "No network. Will sync later.");
+    }
+  }
+  Future<void> pushHabitToCloud() async{
+    if (await _isNetworkAvailable() && _onlineRepo.userID != null) {
+      try {
+        final user = _auth.currentUser;
+        if(user == null){
+          log("Error: User not logged in");
+          return;
+        }
+      await _offlineRepo.fetchhabits();
+            List<Habit> habits = _offlineRepo.currenthabits;
+        for (var habit in habits) {
+          if(habit.userEmail == "local_user"){
+            habit.userEmail = user.email;
+            _onlineRepo.addHabitToFirebase(habit);
+          }
+        }
+      } catch (e) {
+        log("Sync failed: $e");
+      }
+    } else {
+      log(_onlineRepo.userID == null ? "Login required to sync" : "No network. Will sync later.");
+    }
+  }
+  Future<void> syncHabit() async {
+    await pullHabitIntoLocal();
+    await pushHabitToCloud();
   }
 }
